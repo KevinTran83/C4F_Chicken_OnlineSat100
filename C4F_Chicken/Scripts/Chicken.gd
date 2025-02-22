df7e9729bb53c695abf3376d4d939ad8a6eaf97d
Kevin S. Tran <tanksense@gmail.com>
2025-02-22 14:00:53 +1100

    Week 3

--- C4F_Chicken/Scripts/Chicken.gd
+++ C4F_Chicken/Scripts/Chicken.gd
@@ -1,13 +1,23 @@
 extends Node2D
 
 @export var speed = 500
+@export var sprite : AnimatedSprite2D
 
 # Updates every frame
 func _process(delta: float) -> void:
-    if Input.is_key_pressed(KEY_D) : position.x += speed * delta;
-    if Input.is_key_pressed(KEY_A) : position.x -= speed * delta;
+    if Input.is_key_pressed(KEY_D) :
+        position.x += speed * delta;
+        sprite.flip_h = false
+    if Input.is_key_pressed(KEY_A) :
+        position.x -= speed * delta;
+        sprite.flip_h = true
     if Input.is_key_pressed(KEY_W) : position.y -= speed * delta;
     if Input.is_key_pressed(KEY_S) : position.y += speed * delta;
     
     position.x = clamp(position.x, 0, get_viewport_rect().size.x)
     position.y = clamp(position.y, 0, get_viewport_rect().size.y)
+    
+    if Input.is_key_pressed(KEY_D) or Input.is_key_pressed(KEY_A)\
+    or Input.is_key_pressed(KEY_W) or Input.is_key_pressed(KEY_S):
+           sprite.play("Running")
+    else : sprite.play("Idle")
---
+++ C4F_Chicken/Scripts/Vehicle.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+@export var speed = 500;
+@export var sprite : AnimatedSprite2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+    sprite.play( sprite.sprite_frames.get_animation_names()[randi() % 4] )
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+    position.y += speed * delta
